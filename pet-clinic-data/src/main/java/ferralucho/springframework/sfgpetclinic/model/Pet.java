9efaf7f33677c7b602d4f146be298c02fd7baa85
Luciano Ferrari <lucianodarioferrari@gmail.com>
2018-12-31 19:09:26 -0300

    Add Base Entity

--- pet-clinic-data/src/main/java/ferralucho/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/ferralucho/springframework/sfgpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package ferralucho.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
     private PetType getType;
     private Owner owner;
     private LocalDate birthDate;
