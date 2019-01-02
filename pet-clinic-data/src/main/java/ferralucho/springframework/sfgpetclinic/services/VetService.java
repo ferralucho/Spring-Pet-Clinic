29bbef820f1ef33d35978d63534bf807e2db0f1f
Luciano Ferrari <lucianodarioferrari@gmail.com>
2019-01-01 22:37:52 -0300

    Refactor service interface to common base interface

---
+++ pet-clinic-data/src/main/java/ferralucho/springframework/sfgpetclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package ferralucho.springframework.sfgpetclinic.services;
+
+
+import ferralucho.springframework.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService extends CrudService<Vet, Long>{
+
+}
+
