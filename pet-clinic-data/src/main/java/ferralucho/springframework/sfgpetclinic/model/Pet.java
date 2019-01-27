0f88f09cc0409525e0b7d1bd12fc2e689bf5516b
Luciano Ferrari <lucianodarioferrari@gmail.com>
2019-01-27 13:21:06 -0300

    Convert to jpa entities

--- pet-clinic-data/src/main/java/ferralucho/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/ferralucho/springframework/sfgpetclinic/model/Pet.java
@@ -1,12 +1,27 @@
 package ferralucho.springframework.sfgpetclinic.model;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity{
-    private PetType petType;
-    private Owner owner;
-    private LocalDate birthDate;
+
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
+    private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
 
     public String getName() {
         return name;
